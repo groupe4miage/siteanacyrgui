ccbc0067a492da989a2255226a10487843148b0f
geoffrey <forestgeoffrey@gmail.com>
2013-04-10 17:04:20 +0200

    version avec le framework cake

---
+++ Controller/UsersController.php
@@ -0,0 +1,9 @@
+<?php
+ class UsersController extends AppController{
+     
+     function signup(){
+         
+     }
+ }
+ 
+?>
---
+++ View/Users/signup.ctp
@@ -0,0 +1,2 @@
+<H2>S'enregister </H2>
+<?php echo $this->Form->create('User');?>
