8143564c3c6ffd2ac9e54fb3a289d55d39307f45
geoffrey <forestgeoffrey@gmail.com>
2013-04-30 11:19:21 +0200

    Gestion du menu et redirection pour les articles.

--- Model/Category.php
+++ Model/Category.php
@@ -1,10 +1,5 @@
 <?php
     class Category extends AppModel{
         public $hasMany = array('Product');
-         public $belongsTo = array(
-            'SubCategory' => array(
-                'fields'    => 'SubCategory.name'
-            )
-        );
     }
 ?>
